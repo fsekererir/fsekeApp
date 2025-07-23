dfc0ba557177e90a7ad3adf1f1ec8a0e816b3fb2
fsekererir <fsekererir@gmail.com>
2025-07-22 21:56:25 -0600

    Create value.ts

---
+++ ui/components/app/snaps/snap-ui-renderer/components/value.ts
@@ -0,0 +1,18 @@
+import { ValueElement } from '@fsekeapp/snaps-sdk/jsx';
+import { mapToTemplate } from '../utils';
+import { UIComponentFactory } from './types';
+
+export const value: UIComponentFactory<ValueElement> = ({
+  element,
+  ...params
+}) => ({
+  element: 'ConfirmInfoRowValueDouble',
+  props: {
+    left: typeof element.props.extra === 'string' ? element.props.extra : undefined,
+    right: typeof element.props.value === 'string' ? element.props.value : undefined,
+  },
+  propComponents: {
+    left: typeof element.props.extra === 'string' ? undefined : mapToTemplate({ element: element.props.extra, ...params }),
+    right: typeof element.props.value === 'string' ? undefined : mapToTemplate({ element: element.props.value, ...params }),
+  },
+});
